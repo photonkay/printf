3ea7d6abe8a4d377865f4b5c5ce6cffb046053b6
Prince <princeasa2005@gmail.com>
2023-09-19 17:11:38 +0000

    Task Submitted

--- convert_to_binary.c
+++ convert_to_binary.c
@@ -3,13 +3,14 @@
 /**
  * binary - outputs binary
  * @format: character string
+ * list: list of arguments
  * Return: number of characters printed
  */
 int handle_binary(const char **format, va_list list)
 {
 	unsigned int num;
 	int count = 0;
-	int bit, i, numBits; 
+	int bit, i, sigBit, numBits = 0;
 	char bitChar;
 
 	if (**format == 'b')
@@ -19,10 +20,14 @@ int handle_binary(const char **format, va_list list)
 		for (i = numBits - 1; i >= 0; i--)
 		{
 			bit = (num >> i) & 1;
-			bitChar = bit + '0';
-			write(1, &bitChar, 1);
+			if (bit || sigBit)
+			{
+				bitChar = bit + '0';
+				write(1, &bitChar, 1);
+				count++;
+				sigBit = 1;
+			}
 		}
-		count += numBits;
 	}
 	else
 	{
