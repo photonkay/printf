c3c8a7a275b2a0cd529e4bfeeedf215b66c48c17
Prince <princeasa2005@gmail.com>
2023-09-21 12:08:47 +0000

    Going through changes

---
+++ testfiles/main_6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code otherwise
+ */
+int main(void)
+{
+	int len, len2;
+
+	len = _printf("%b", 1024);
+	len2 = printf("10000000000");
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	return (0);
+}
